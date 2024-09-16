afc136967283e234824779a4f7944d93d2660345
huwei <18586626673@163.com>
2024-09-16 19:43:27 +0800

    init project

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,13 @@
 // tailwind config is required for editor support
 
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from '@repo/tailwind-config';
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx"],
-  presets: [sharedConfig],
+import type { Config } from 'tailwindcss';
+
+const config: Pick<Config, 'content' | 'presets' | 'prefix'> = {
+  content: ['./src/app/**/*.tsx'],
+  prefix: 't-',
+  presets: [sharedConfig]
 };
 
 export default config;
---
+++ eslint.config.mjs
@@ -0,0 +1,110 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['node_modules/**', '**/*.stories.tsx', '**/postcss.config.js', 'next.config.js'] },
+  { files: ['**/*.{js,mjs,cjs,ts,tsx}'] },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      ts,
+      // eslint-disable-next-line sort-keys
+      '@typescript-eslint': ts,
+      import: importPlugin,
+      prettier: eslintPluginPrettier
+    }
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  //  Sort import
+  {
+    rules: {
+      'import/no-self-import': 'error',
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            caseInsensitive: true,
+            order: 'asc'
+          },
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              group: 'external',
+              pattern: 'react',
+              position: 'before'
+            },
+            {
+              group: 'external',
+              pattern: 'next/**',
+              position: 'before'
+            },
+            {
+              group: 'external',
+              pattern: '@zenni/**',
+              position: 'after'
+            },
+            {
+              group: 'internal',
+              pattern: '@/**',
+              position: 'after'
+            },
+            {
+              group: 'type',
+              pattern: '*.scss',
+              patternOptions: { matchBase: true },
+              position: 'after'
+            }
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          warnOnUnassignedImports: true
+        }
+      ],
+      'no-duplicate-imports': 'error',
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false
+        }
+      ]
+    }
+  },
+  {
+    rules: {
+      eqeqeq: 'error',
+      'no-alert': 'error',
+      'no-empty': 'error',
+      'no-undef': 'error',
+      'no-unused-vars': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prettier/prettier': 'error',
+      'sort-keys': [
+        'error',
+        'asc',
+        {
+          allowLineSeparatedGroups: true,
+          caseSensitive: true,
+          minKeys: 2,
+          natural: true
+        }
+      ]
+    }
+  }
+];
